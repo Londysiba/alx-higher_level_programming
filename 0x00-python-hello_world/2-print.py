ed7f2a4cf62bd31e6a15bb538f5df597ada4c27c
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-06-05 14:03:51 +0200

    python

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 
-print('"Programming is like building a multilingual puzzle')
-
-with open('2-print.py', 'a') as file:
-    file.write('\n')
+print("\"Programming is like building a multilingual puzzle")
--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
+number = 98
 print(f"{98} Battery street")
-
-with open("3-print_number.py", "a") as file:
-    file.write("\n")

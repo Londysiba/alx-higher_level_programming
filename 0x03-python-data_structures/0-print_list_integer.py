b1cf6c27ca9c73d85388c7de9660ea9aedb3a0b3
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-06-11 18:59:33 +0200

    list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    a = 1
-
-    while a < 6:
-        print(format(a))
-        a = a+1
+    for n in my_list:
+        print("{}".format(n))

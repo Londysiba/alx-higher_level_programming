1d05dcdfe21d85be6c762ff36086f1ee2c06f87a
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-09-12 15:02:27 +0200

    3-rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) { [this.width, this.height] = [width, height]; }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};

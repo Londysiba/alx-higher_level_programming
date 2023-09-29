b3126867ffb1409273958c9642a5209fbeb0d341
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-09-29 15:49:26 +0200

    task4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
 
     print('Body response:')
     print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    print('\t- response: {}'.format(response.text))

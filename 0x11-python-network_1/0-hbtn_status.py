10f022ffac6f44d1fec30ac1b778dacbadd54d1d
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-09-29 15:48:15 +0200

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 
-from urllib import request
+
+import urllib.request
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         response = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
+    print('Body response:')
+    print('\t- type: {}'.format(type(response)))
+    print('\t- response: {}'.format(response))
+    print('\t- utf8 response: {}'.format(str(response)[2:-1]))

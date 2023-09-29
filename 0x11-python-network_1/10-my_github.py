6a9608725b8b5eccfd5d7672d7381fa4c9b4e678
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-09-29 15:29:44 +0200

    task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Uses the GitHub API to display a GitHub ID based on given credentials."""
 
-import sys
 import requests
-from requests.auth import HTTPBasicAuth
+from sys import argv
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    url = 'https://api.github.com/user'
+    user = argv[1]
+    token = argv[2]
+    headers = {'Authorization': 'token {}'.format(token)}
+
+    r = requests.get(url, headers=headers)
+
+    print(r.json().get('id'))

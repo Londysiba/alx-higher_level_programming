33e2418202a76e9bbd9b3c471d91dd032db27781
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-09-29 14:56:53 +0200

    changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 
-import urllib
+from urllib import request
 
 if __name__ == "__main__":
     with request.urlopen("https://intranet.hbtn.io/status") as response:
